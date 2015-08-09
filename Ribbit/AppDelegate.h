065cd4febf44e2f7aba64099b012057a539b669c
joshmotley <joshkmotley@gmail.com>
2015-08-09 14:17:38 -0400

    Lollipop

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Joshua Motley on 8/8/15.
+//  Copyright (c) 2015 TeamTreeHouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ribbit
+//
+//  Created by Joshua Motley on 8/8/15.
+//  Copyright (c) 2015 TeamTreeHouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
