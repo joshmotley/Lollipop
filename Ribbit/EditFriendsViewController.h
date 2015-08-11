26cd545c9a9209e580dcb2bab4ae9a4725f345e8
joshmotley <joshkmotley@gmail.com>
2015-08-11 16:36:17 -0400

    add edit friends table that fills table with all users from query

---
+++ Ribbit/EditFriendsViewController.h
@@ -0,0 +1,15 @@
+//
+//  EditFriendsViewController.h
+//  Ribbit
+//
+//  Created by Joshua Motley on 8/11/15.
+//  Copyright (c) 2015 TeamTreeHouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditFriendsViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *allUsers;
+
+@end
---
+++ Ribbit/EditFriendsViewController.m
@@ -0,0 +1,83 @@
+//
+//  EditFriendsViewController.m
+//  Ribbit
+//
+//  Created by Joshua Motley on 8/11/15.
+//  Copyright (c) 2015 TeamTreeHouse. All rights reserved.
+//
+
+#import "EditFriendsViewController.h"
+#import <Parse/Parse.h>
+
+@interface EditFriendsViewController ()
+
+@end
+
+@implementation EditFriendsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    PFQuery *query = [PFUser query];
+    [query orderByAscending:@"username"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error){
+        if(error){
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+        else{
+            self.allUsers = objects;
+            
+            // Let's table know there is more data to add
+            [self.tableView reloadData];
+            
+        }
+    }];
+    
+}
+
+
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    // Return the number of rows in the section.
+    return self.allUsers.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    PFUser *user = [self.allUsers objectAtIndex:indexPath.row];
+    cell.textLabel.text = user.username;
+    
+    
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
